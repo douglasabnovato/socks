cd6ccbd1f593b03fc6595e308f69760dd651a4b6
@douglasabnovato <douglasabnovato@gmail.com>
2020-04-23 20:53:41 -0300

    img socks

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue ({
     el: '#app',
     data: {
         product: 'Douglas A B Novato',
-        description: '@douglasabnovato'
+        description: '@douglasabnovato',
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'        
     }
 })
