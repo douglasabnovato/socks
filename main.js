af9ac1e25ad3a274de009e90fa3c5a3f238714a4
@douglasabnovato <douglasabnovato@gmail.com>
2020-04-29 17:13:16 -0300

    Conditional Rendering

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var app = new Vue ({
         description: '@douglasabnovato',
         altText: 'a pair of socks',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        inStock: true,
+        inventory: 0,
         image: './images/vmSocks-green-onWhite.jpg'
         /*'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'*/
     }
