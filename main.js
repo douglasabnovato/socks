9be8a379407d8b01624ce467b921509dcc3a12be
@douglasabnovato <douglasabnovato@gmail.com>
2020-04-29 19:06:17 -0300

    v-for

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var app = new Vue ({
     el: '#app',
     data: {
-        product: 'Douglas A B Novato',
-        description: '@douglasabnovato',
+        product: 'Socks',
+        description: 'Style and Comfort',
         altText: 'a pair of socks',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inStock: true,
         inventory: 0,
         onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
         image: './images/vmSocks-green-onWhite.jpg'
         /*'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'*/
     }
