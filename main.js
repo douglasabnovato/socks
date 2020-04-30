7a04c03fe2bc3608f8ba0cee2f944f2ccce24a44
@douglasabnovato <douglasabnovato@gmail.com>
2020-04-30 18:59:34 -0300

    selectedVariant

--- main.js
+++ main.js
@@ -1,40 +1,52 @@
 var app = new Vue ({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
+        selectedVariant: 0,
         description: 'Style and Comfort',
         altText: 'a pair of socks',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inStock: false,
         inventory: 0,
-        onSale: true,
+        onSale: true,                
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        details: ["80% cotton", "20% polyester", "Gender-neutral"], 
         variants: [
             {
                 variantId: 2234,
                 variantColor: "Green",
-                variantImage: './images/vmSocks-green-onWhite.jpg'
+                variantImage: './images/vmSocks-green-onWhite.jpg',
+                variantQuantity: 0
             },
             {
                 variantId: 2235,
                 variantColor: "Blue",
-                variantImage: './images/vmSocks-blue-onWhite.jpg'
+                variantImage: './images/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
-        cart: 0,        
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        details: ["80% cotton", "20% polyester", "Gender-neutral"], 
-        image: './images/vmSocks-green-onWhite.jpg'
-        /*'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'*/
-    },
+        cart: 0,
+    },    
     methods: {
         addToCart: function () {
             this.cart += 1 
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
           },
         removeFromCart() {
             this.cart -= 1
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage           
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
