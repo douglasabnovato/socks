e04cec4b748f89da2e238dc60816ab7c31585cee
@douglasabnovato <douglasabnovato@gmail.com>
2020-04-30 00:18:51 -0300

    event handling

--- main.js
+++ main.js
@@ -11,17 +11,27 @@ var app = new Vue ({
         variants: [
             {
                 variantId: 2234,
-                variantColor: "Green"
+                variantColor: "Green",
+                variantImage: './images/vmSocks-green-onWhite.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: "Blue"
+                variantColor: "Blue",
+                variantImage: './images/vmSocks-blue-onWhite.jpg'
             }
         ],
-        cart: 0,
+        cart: 0,        
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         details: ["80% cotton", "20% polyester", "Gender-neutral"], 
         image: './images/vmSocks-green-onWhite.jpg'
         /*'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'*/
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1 
+        },
+        updateProduct: function (variantImage) {
+            this.image = variantImage
+        }
     }
 })
