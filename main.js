511020ac9af399e9f8874e845063d95a880b4866
@douglasabnovato <douglasabnovato@gmail.com>
2020-05-05 18:29:20 -0300

    forms-template

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+Vue.component('product-review',{
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+        <p>
+            <label for="name">Name: </label>
+            <input id="name" v-model="name" placeholder="name">
+        </p>
+        <p>
+            <label for="review">Review:</label>
+            <textarea id="review" v-model="review"></textarea>
+        </p>
+        <p>
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating">
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+        </p>
+
+        </form>
+    `,
+    data(){
+        return {
+            name: "douglasabnovato"
+        }
+    }
+})
+
 Vue.component('product-details',{
     props: {
         details:{
