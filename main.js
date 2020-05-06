795c0e0aff0b0c32318cc954086bb613eacff045
@douglasabnovato <douglasabnovato@gmail.com>
2020-05-05 21:06:04 -0300

    form - v-model

--- main.js
+++ main.js
@@ -41,6 +41,20 @@ Vue.component('product', {
                 <p><a :href="link" target="_blank">More products like this</a></p>
                 
             </div> 
+
+            <div>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul v-else>
+                    <li v-for="(review, index) in reviews" :key="index">
+                        <p>{{ review.name }}</p>
+                        <p>Rating: {{ review.rating }}</p>
+                        <p>{{ review.review }}</p>
+                    </li>
+                </ul>
+            </div>
+
+            <product-review @review-submitted="addReview"></product-review>
+
         </div>
     `,
     data() {
@@ -65,6 +79,7 @@ Vue.component('product', {
                     variantQuantity: 0
                 }
             ],
+            reviews:[],
             onSale: true
         }
     },
@@ -77,7 +92,10 @@ Vue.component('product', {
         },
         removeFromCart() {
             this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
-        }
+        },
+        addReview(productReview){
+            this.reviews.push(productReview)
+        }   
     },
     computed: {
         title() {
@@ -107,14 +125,24 @@ Vue.component('product', {
 Vue.component('product-review',{
     template: `
         <form class="review-form" @submit.prevent="onSubmit">
+            
+            <p class="error" v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
+
             <p>
                 <label for="name">Name: </label>
                 <input id="name" v-model="name" placeholder="name">
             </p>
+
             <p>
                 <label for="review">Review:</label>
                 <textarea id="review" v-model="review"></textarea>
             </p>
+
             <p>
                 <label for="rating">Rating:</label>
                 <select id="rating" v-model.number="rating">
@@ -125,31 +153,51 @@ Vue.component('product-review',{
                     <option>1</option>
                 </select>
             </p>
+
+            <p>Would you recommend this product?</p>
+            <label>
+                Yes
+                <input type="radio" value="Yes" v-model="recommend"/>
+            </label>
+            <label>
+                No
+                <input type="radio" value="No" v-model="recommend"/>
+            </label>
             <p>
                 <input type="submit" value="Submit">
             </p>
+
         </form>
     `,
     data(){
         return {
             name: null,
             review: [],
-            rating: null
+            rating: null,
+            errors:[]
         }
     },
     methods:{
         onSubmit(){
-            let productReview = {
-                name: this.name,
-                review: this.review,
-                rating: this.rating
-            }
+            this.errors = []
+            if(this.name && this.review && this.rating){
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+            }else{
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+            }          
             this.name=null,
             this.review=null,
             this.rating=null
-        },
-        addReview(productReview){
-            this.reviews.push(productReview)
         }
     }
 })
